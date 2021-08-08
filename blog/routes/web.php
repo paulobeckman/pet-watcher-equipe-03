d58583242e5feb74f793368c4edd76c95c809c1a
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-08-08 02:07:11 -0300

    [Login/Especies/Reset Senha] Tela de Login/Registro Tela de Especies/CRUD Tela de Reset Senha

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
+
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', 'AuthController@index');
+Route::post('post-login', 'AuthController@postLogin'); 
+Route::get('register', 'AuthController@register');
+Route::post('post-register', 'AuthController@postRegister'); 
+Route::get('dashboard', 'AuthController@dashboard'); 
+Route::get('logout', 'AuthController@logout');
+
+Route::get('home', 'HomeController@index');
+
+Route::get('species', 'SpeciesController@index');
+Route::get('species/create', 'SpeciesController@create');
+Route::post('species/create', 'SpeciesController@store')->name('nova_especie');
+Route::get('species/{id}', 'SpeciesController@show');
+Route::get('species/edit/{id}', 'SpeciesController@edit');
+Route::post('species/edit/{id}', 'SpeciesController@update');
+Route::delete('species/{id}', 'SpeciesController@destroy');
+
+Route::get('settings/change-password', 'AuthController@change_password')->name('change_password');
+Route::post('settings/update-password', 'AuthController@update_password')->name('update_password');
+
+
